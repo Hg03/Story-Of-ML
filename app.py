d41cb155a0bb96b932817ffda1836fb1deb6a807
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-04-29 12:27:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
   
-st.set_page_config(page_icon = '🔑',initial_sidebar_state='expanded',menu_items={'Get Help':'gehloth03@gmail.com','Report a bug':'https://github.com/Hg03/Story-Of-ML','About':'Intuition on Algorithms 🖋️'}) 
+st.set_page_config(page_icon = '🔑',initial_sidebar_state='expanded',menu_items={'Get Help':'https://www.linkedin.com/in/harish-gehlot-5338a021a/','Report a bug':'https://github.com/Hg03/Story-Of-ML','About':'Intuition on Algorithms 🖋️'}) 
 st.title('Story On ML')
 st.sidebar.title('Articles')
 articles_path = {'Linear Regression':'Linear-Regression.md','Logistic Regression':'Logistic-Regression.md','Support Vector Machine':'Support-Vector-Machine.md'}
